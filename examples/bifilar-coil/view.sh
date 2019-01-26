81e5602c9980bd65348fbeb9b3b6a73af000c265
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-01-26 11:51:27 -0500

    Fix view.sh

--- examples/bifilar-coil/view.sh
+++ examples/bifilar-coil/view.sh
@@ -1,36 +1,36 @@
 #!/bin/bash -ex
 go run main.go -font "aaarghnormal"
 mv bifilar-coil.zip bifilar-coil.aaarghnormal.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "fascinate_inlineregular"
 mv bifilar-coil.zip bifilar-coil.fascinate_inlineregular.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "gooddogregular"
 mv bifilar-coil.zip bifilar-coil.gooddogregular.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "helsinkiregular"
 mv bifilar-coil.zip bifilar-coil.helsinkiregular.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "latoregular"
 mv bifilar-coil.zip bifilar-coil.latoregular.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "overlockregular"
 mv bifilar-coil.zip bifilar-coil.overlockregular.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "ubuntumonoregular"
 mv bifilar-coil.zip bifilar-coil.ubuntumonoregular.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "webfontb8enx5qc"
 mv bifilar-coil.zip bifilar-coil.webfontb8enx5qc.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
 
 go run main.go -font "webfontrtffmgjf"
 mv bifilar-coil.zip bifilar-coil.webfontrtffmgjf.zip
-gerbview bifilar-coil.g* bifilar-coil.xln
+gerbview bifilar-coil.gbl bifilar-coil.gbs bifilar-coil.gtl bifilar-coil.gts bifilar-coil.gbo bifilar-coil.gko bifilar-coil.gto bifilar-coil.xln
