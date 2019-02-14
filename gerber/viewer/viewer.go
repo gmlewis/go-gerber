10607b320e1aa70c36ab8a625fa118662ceaa3b1
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-02-13 23:09:10 -0500

    Add checkboxes for all layers

--- gerber/viewer/viewer.go
+++ gerber/viewer/viewer.go
@@ -14,57 +14,108 @@ import (
 )
 
 type viewController struct {
-	g     *gerber.Gerber
-	lastW int
-	lastH int
-	scale float64
+	g         *gerber.Gerber
+	lastW     int
+	lastH     int
+	scale     float64
+	drawLayer []bool
+
+	indexTopSilkscreen    int
+	indexTopSolderMask    int
+	indexTop              int
+	indexLayer2           int
+	indexLayer3           int
+	indexLayer4           int
+	indexLayer5           int
+	indexBottom           int
+	indexBottomSilkscreen int
+	indexBottomSolderMask int
+	indexDrill            int
+	indexOutline          int
+}
+
+func initController(g *gerber.Gerber) *viewController {
+	vc := &viewController{
+		g:                     g,
+		drawLayer:             make([]bool, len(g.Layers)),
+		indexTopSilkscreen:    -1,
+		indexTopSolderMask:    -1,
+		indexTop:              -1,
+		indexLayer2:           -1,
+		indexLayer3:           -1,
+		indexLayer4:           -1,
+		indexLayer5:           -1,
+		indexBottom:           -1,
+		indexBottomSilkscreen: -1,
+		indexBottomSolderMask: -1,
+		indexDrill:            -1,
+		indexOutline:          -1,
+	}
+
+	for i, layer := range g.Layers {
+		vc.drawLayer[i] = true
+		switch layer.Filename[len(layer.Filename)-3:] {
+		case "gtl":
+			vc.indexTop = i
+		case "gts":
+			vc.indexTopSolderMask = i
+		case "gto":
+			vc.indexTopSilkscreen = i
+		case "gbl":
+			vc.indexBottom = i
+		case "gbs":
+			vc.indexBottomSolderMask = i
+		case "gbo":
+			vc.indexBottomSilkscreen = i
+		case "g2l":
+			vc.indexLayer2 = i
+		case "g3l":
+			vc.indexLayer3 = i
+		case "g4l":
+			vc.indexLayer4 = i
+		case "g5l":
+			vc.indexLayer5 = i
+		case "xln":
+			vc.indexDrill = i
+		case "gko":
+			vc.indexOutline = i
+		default:
+			log.Fatalf("Unknown Gerber layer: %v", layer.Filename)
+		}
+	}
+	return vc
 }
 
 func Gerber(g *gerber.Gerber) {
 	a := app.New()
 
-	vc := &viewController{g: g}
+	vc := initController(g)
 	c := canvas.NewRaster(vc.pixelFunc)
 	c.SetMinSize(fyne.Size{Width: 800, Height: 800})
 
-	layers := widget.NewVBox(
-		widget.NewHBox(
-			widget.NewCheck("Top Silkscreen", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Top", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Layer 2", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Layer 3", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Layer 4", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Layer 5", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Bottom", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Bottom Silkscreen", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-		widget.NewHBox(
-			widget.NewCheck("Drill", func(v bool) {}),
-			layout.NewSpacer(),
-		),
-	)
+	layers := widget.NewVBox()
+	addCheck := func(index int, label string) {
+		if index >= 0 {
+			check := widget.NewCheck(label, func(v bool) {
+				vc.drawLayer[index] = v
+				canvas.Refresh(c)
+			})
+			check.SetChecked(true)
+			layers.Append(widget.NewHBox(check, layout.NewSpacer()))
+		}
+	}
+	addCheck(vc.indexTopSilkscreen, "Top Silkscreen")
+	addCheck(vc.indexTopSolderMask, "Top Solder Mask")
+	addCheck(vc.indexTop, "Top")
+	addCheck(vc.indexLayer2, "Layer 2")
+	addCheck(vc.indexLayer3, "Layer 3")
+	addCheck(vc.indexLayer4, "Layer 4")
+	addCheck(vc.indexLayer5, "Layer 5")
+	addCheck(vc.indexBottom, "Bottom")
+	addCheck(vc.indexBottomSolderMask, "Bottom Solder Mask")
+	addCheck(vc.indexBottomSilkscreen, "Bottom Silkscreen")
+	addCheck(vc.indexDrill, "Drill")
+	addCheck(vc.indexOutline, "Outline")
 	quit := widget.NewHBox(
 		layout.NewSpacer(),
 		widget.NewButton("Quit", func() { a.Quit() }),
