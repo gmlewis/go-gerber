6bbe43794d839b4b6f66d7184364684dd6422a42
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-02-18 17:42:57 -0500

    Gen layers in a loop

--- examples/icosi-bifilar-coil/main.go
+++ examples/icosi-bifilar-coil/main.go
@@ -29,8 +29,8 @@ var (
 )
 
 const (
-	ncoils     = 20
-	angleDelta = 2.0 * math.Pi / ncoils
+	nlayers    = 20
+	angleDelta = 2.0 * math.Pi / nlayers
 
 	messageFmt = `Trace size = %0.2fmm.
 Gap size = %0.2fmm.
@@ -75,57 +75,33 @@ func main() {
 	startBotR, botSpiralR, endBotR := s.genSpiral(-1, 0, 0)
 	startBotL, botSpiralL, endBotL := s.genSpiral(-1, math.Pi, *trace+padD)
 
-	startLayer2R, layer2SpiralR, endLayer2R := s.genSpiral(1, angleDelta, 0)
-	startLayer2L, layer2SpiralL, endLayer2L := s.genSpiral(1, math.Pi+angleDelta, 0)
-	startLayer3R, layer3SpiralR, endLayer3R := s.genSpiral(-1, angleDelta, 0)
-	startLayer3L, layer3SpiralL, endLayer3L := s.genSpiral(-1, math.Pi+angleDelta, 0)
+	startLayerNR := map[int]Pt{}
+	startLayerNL := map[int]Pt{}
+	endLayerNR := map[int]Pt{}
+	endLayerNL := map[int]Pt{}
+	layerNSpiralR := map[int][]Pt{}
+	layerNSpiralL := map[int][]Pt{}
+	for n := 2; n < nlayers; n += 4 {
+		af := float64((n + 2) / 4)
+		startLayerNR[n], layerNSpiralR[n], endLayerNR[n] = s.genSpiral(1, af*angleDelta, 0)
+		startLayerNL[n], layerNSpiralL[n], endLayerNL[n] = s.genSpiral(1, math.Pi+af*angleDelta, 0)
+		startLayerNR[n+1], layerNSpiralR[n+1], endLayerNR[n+1] = s.genSpiral(-1, af*angleDelta, 0)
+		startLayerNL[n+1], layerNSpiralL[n+1], endLayerNL[n+1] = s.genSpiral(-1, math.Pi+af*angleDelta, 0)
 
-	startLayer4R, layer4SpiralR, endLayer4R := s.genSpiral(1, -angleDelta, 0)
-	startLayer4L, layer4SpiralL, endLayer4L := s.genSpiral(1, math.Pi-angleDelta, 0)
-	startLayer5R, layer5SpiralR, endLayer5R := s.genSpiral(-1, -angleDelta, 0)
-	startLayer5L, layer5SpiralL, endLayer5L := s.genSpiral(-1, math.Pi-angleDelta, 0)
-
-	startLayer6R, layer6SpiralR, endLayer6R := s.genSpiral(1, 2*angleDelta, 0)
-	startLayer6L, layer6SpiralL, endLayer6L := s.genSpiral(1, math.Pi+2*angleDelta, 0)
-	startLayer7R, layer7SpiralR, endLayer7R := s.genSpiral(-1, 2*angleDelta, 0)
-	startLayer7L, layer7SpiralL, endLayer7L := s.genSpiral(-1, math.Pi+2*angleDelta, 0)
-
-	startLayer8R, layer8SpiralR, endLayer8R := s.genSpiral(1, -2*angleDelta, 0)
-	startLayer8L, layer8SpiralL, endLayer8L := s.genSpiral(1, math.Pi-2*angleDelta, 0)
-	startLayer9R, layer9SpiralR, endLayer9R := s.genSpiral(-1, -2*angleDelta, 0)
-	startLayer9L, layer9SpiralL, endLayer9L := s.genSpiral(-1, math.Pi-2*angleDelta, 0)
-
-	startLayer10R, layer10SpiralR, endLayer10R := s.genSpiral(1, 3*angleDelta, 0)
-	startLayer10L, layer10SpiralL, endLayer10L := s.genSpiral(1, math.Pi+3*angleDelta, 0)
-	startLayer11R, layer11SpiralR, endLayer11R := s.genSpiral(-1, 3*angleDelta, 0)
-	startLayer11L, layer11SpiralL, endLayer11L := s.genSpiral(-1, math.Pi+3*angleDelta, 0)
-
-	startLayer12R, layer12SpiralR, endLayer12R := s.genSpiral(1, -3*angleDelta, 0)
-	startLayer12L, layer12SpiralL, endLayer12L := s.genSpiral(1, math.Pi-3*angleDelta, 0)
-	startLayer13R, layer13SpiralR, endLayer13R := s.genSpiral(-1, -3*angleDelta, 0)
-	startLayer13L, layer13SpiralL, endLayer13L := s.genSpiral(-1, math.Pi-3*angleDelta, 0)
-
-	startLayer14R, layer14SpiralR, endLayer14R := s.genSpiral(1, 4*angleDelta, 0)
-	startLayer14L, layer14SpiralL, endLayer14L := s.genSpiral(1, math.Pi+4*angleDelta, 0)
-	startLayer15R, layer15SpiralR, endLayer15R := s.genSpiral(-1, 4*angleDelta, 0)
-	startLayer15L, layer15SpiralL, endLayer15L := s.genSpiral(-1, math.Pi+4*angleDelta, 0)
-
-	startLayer16R, layer16SpiralR, endLayer16R := s.genSpiral(1, -4*angleDelta, 0)
-	startLayer16L, layer16SpiralL, endLayer16L := s.genSpiral(1, math.Pi-4*angleDelta, 0)
-	startLayer17R, layer17SpiralR, endLayer17R := s.genSpiral(-1, -4*angleDelta, 0)
-	startLayer17L, layer17SpiralL, endLayer17L := s.genSpiral(-1, math.Pi-4*angleDelta, 0)
-
-	startLayer18R, layer18SpiralR, endLayer18R := s.genSpiral(1, -5*angleDelta, 0)
-	startLayer18L, layer18SpiralL, endLayer18L := s.genSpiral(1, math.Pi-5*angleDelta, 0)
-	startLayer19R, layer19SpiralR, endLayer19R := s.genSpiral(-1, -5*angleDelta, 0)
-	startLayer19L, layer19SpiralL, endLayer19L := s.genSpiral(-1, math.Pi-5*angleDelta, 0)
+		if n+2 < nlayers {
+			startLayerNR[n+2], layerNSpiralR[n+2], endLayerNR[n+2] = s.genSpiral(1, -af*angleDelta, 0)
+			startLayerNL[n+2], layerNSpiralL[n+2], endLayerNL[n+2] = s.genSpiral(1, math.Pi-af*angleDelta, 0)
+			startLayerNR[n+3], layerNSpiralR[n+3], endLayerNR[n+3] = s.genSpiral(-1, -af*angleDelta, 0)
+			startLayerNL[n+3], layerNSpiralL[n+3], endLayerNL[n+3] = s.genSpiral(-1, math.Pi-af*angleDelta, 0)
+		}
+	}
 
 	viaPadD := 0.5
 	innerR := (*gap + viaPadD) / math.Sin(angleDelta)
 	// minStartAngle := (innerR + *gap + 0.5**trace + 0.5*viaPadD) * (3 * math.Pi)
 	// log.Printf("innerR=%v, minStartAngle=%v/Pi=%v", innerR, minStartAngle, minStartAngle/math.Pi)
 	var innerViaPts []Pt
-	for i := 0; i < ncoils; i++ {
+	for i := 0; i < nlayers; i++ {
 		x := innerR * math.Cos(float64(i)*angleDelta)
 		y := innerR * math.Sin(float64(i)*angleDelta)
 		innerViaPts = append(innerViaPts, Pt{x, y})
@@ -182,7 +158,7 @@ func main() {
 	}
 
 	var outerViaPts []Pt
-	for i := 0; i < ncoils; i++ {
+	for i := 0; i < nlayers; i++ {
 		pt := outerContactPt(float64(i))
 		outerViaPts = append(outerViaPts, pt)
 	}
@@ -266,104 +242,21 @@ func main() {
 	topMask := g.TopSolderMask()
 	addVias(topMask)
 
-	layer2 := g.LayerN(2)
-	layer2.Add(
-		Polygon(Pt{0, 0}, true, layer2SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer2SpiralL, 0.0),
-		padLine(startLayer2R, innerViaPts[innerHole["2R"]]),
-		padLine(startLayer2L, innerViaPts[innerHole["2L"]]),
-		padLine(endLayer2R, outerViaPts[outerHole["2R"]]),
-		padLine(endLayer2L, outerViaPts[outerHole["2L"]]),
-	)
-	addVias(layer2)
-
-	layer4 := g.LayerN(4)
-	layer4.Add(
-		Polygon(Pt{0, 0}, true, layer4SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer4SpiralL, 0.0),
-		padLine(startLayer4R, innerViaPts[innerHole["4R"]]),
-		padLine(startLayer4L, innerViaPts[innerHole["4L"]]),
-		padLine(endLayer4R, outerViaPts[outerHole["4R"]]),
-		padLine(endLayer4L, outerViaPts[outerHole["4L"]]),
-	)
-	addVias(layer4)
-
-	layer6 := g.LayerN(6)
-	layer6.Add(
-		Polygon(Pt{0, 0}, true, layer6SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer6SpiralL, 0.0),
-		padLine(startLayer6R, innerViaPts[innerHole["6R"]]),
-		padLine(startLayer6L, innerViaPts[innerHole["6L"]]),
-		padLine(endLayer6R, outerViaPts[outerHole["6R"]]),
-		padLine(endLayer6L, outerViaPts[outerHole["6L"]]),
-	)
-	addVias(layer6)
-
-	layer8 := g.LayerN(8)
-	layer8.Add(
-		Polygon(Pt{0, 0}, true, layer8SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer8SpiralL, 0.0),
-		padLine(startLayer8R, innerViaPts[innerHole["8R"]]),
-		padLine(startLayer8L, innerViaPts[innerHole["8L"]]),
-		padLine(endLayer8R, outerViaPts[outerHole["8R"]]),
-		padLine(endLayer8L, outerViaPts[outerHole["8L"]]),
-	)
-	addVias(layer8)
-
-	layer10 := g.LayerN(10)
-	layer10.Add(
-		Polygon(Pt{0, 0}, true, layer10SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer10SpiralL, 0.0),
-		padLine(startLayer10R, innerViaPts[innerHole["10R"]]),
-		padLine(startLayer10L, innerViaPts[innerHole["10L"]]),
-		padLine(endLayer10R, outerViaPts[outerHole["10R"]]),
-		padLine(endLayer10L, outerViaPts[outerHole["10L"]]),
-	)
-	addVias(layer10)
-
-	layer12 := g.LayerN(12)
-	layer12.Add(
-		Polygon(Pt{0, 0}, true, layer12SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer12SpiralL, 0.0),
-		padLine(startLayer12R, innerViaPts[innerHole["12R"]]),
-		padLine(startLayer12L, innerViaPts[innerHole["12L"]]),
-		padLine(endLayer12R, outerViaPts[outerHole["12R"]]),
-		padLine(endLayer12L, outerViaPts[outerHole["12L"]]),
-	)
-	addVias(layer12)
-
-	layer14 := g.LayerN(14)
-	layer14.Add(
-		Polygon(Pt{0, 0}, true, layer14SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer14SpiralL, 0.0),
-		padLine(startLayer14R, innerViaPts[innerHole["14R"]]),
-		padLine(startLayer14L, innerViaPts[innerHole["14L"]]),
-		padLine(endLayer14R, outerViaPts[outerHole["14R"]]),
-		padLine(endLayer14L, outerViaPts[outerHole["14L"]]),
-	)
-	addVias(layer14)
-
-	layer16 := g.LayerN(16)
-	layer16.Add(
-		Polygon(Pt{0, 0}, true, layer16SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer16SpiralL, 0.0),
-		padLine(startLayer16R, innerViaPts[innerHole["16R"]]),
-		padLine(startLayer16L, innerViaPts[innerHole["16L"]]),
-		padLine(endLayer16R, outerViaPts[outerHole["16R"]]),
-		padLine(endLayer16L, outerViaPts[outerHole["16L"]]),
-	)
-	addVias(layer16)
-
-	layer18 := g.LayerN(18)
-	layer18.Add(
-		Polygon(Pt{0, 0}, true, layer18SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer18SpiralL, 0.0),
-		padLine(startLayer18R, innerViaPts[innerHole["18R"]]),
-		padLine(startLayer18L, innerViaPts[innerHole["18L"]]),
-		padLine(endLayer18R, outerViaPts[outerHole["18R"]]),
-		padLine(endLayer18L, outerViaPts[outerHole["18L"]]),
-	)
-	addVias(layer18)
+	for n := 2; n < nlayers; n++ {
+		log.Printf("Generating layer %v...", n)
+		nr := fmt.Sprintf("%vR", n)
+		nl := fmt.Sprintf("%vL", n)
+		layer := g.LayerN(n)
+		layer.Add(
+			Polygon(Pt{0, 0}, true, layerNSpiralR[n], 0.0),
+			Polygon(Pt{0, 0}, true, layerNSpiralL[n], 0.0),
+			padLine(startLayerNR[n], innerViaPts[innerHole[nr]]),
+			padLine(startLayerNL[n], innerViaPts[innerHole[nl]]),
+			padLine(endLayerNR[n], outerViaPts[outerHole[nr]]),
+			padLine(endLayerNL[n], outerViaPts[outerHole[nl]]),
+		)
+		addVias(layer)
+	}
 
 	bottom := g.BottomCopper()
 	bottom.Add(
@@ -378,105 +271,6 @@ func main() {
 
 	bottomMask := g.BottomSolderMask()
 	addVias(bottomMask)
-
-	layer3 := g.LayerN(3)
-	layer3.Add(
-		Polygon(Pt{0, 0}, true, layer3SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer3SpiralL, 0.0),
-		padLine(startLayer3R, innerViaPts[innerHole["3R"]]),
-		padLine(startLayer3L, innerViaPts[innerHole["3L"]]),
-		padLine(endLayer3R, outerViaPts[outerHole["3R"]]),
-		padLine(endLayer3L, outerViaPts[outerHole["3L"]]),
-	)
-	addVias(layer3)
-
-	layer5 := g.LayerN(5)
-	layer5.Add(
-		Polygon(Pt{0, 0}, true, layer5SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer5SpiralL, 0.0),
-		padLine(startLayer5R, innerViaPts[innerHole["5R"]]),
-		padLine(startLayer5L, innerViaPts[innerHole["5L"]]),
-		padLine(endLayer5R, outerViaPts[outerHole["5R"]]),
-		padLine(endLayer5L, outerViaPts[outerHole["5L"]]),
-	)
-	addVias(layer5)
-
-	layer7 := g.LayerN(7)
-	layer7.Add(
-		Polygon(Pt{0, 0}, true, layer7SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer7SpiralL, 0.0),
-		padLine(startLayer7R, innerViaPts[innerHole["7R"]]),
-		padLine(startLayer7L, innerViaPts[innerHole["7L"]]),
-		padLine(endLayer7R, outerViaPts[outerHole["7R"]]),
-		padLine(endLayer7L, outerViaPts[outerHole["7L"]]),
-	)
-	addVias(layer7)
-
-	layer9 := g.LayerN(9)
-	layer9.Add(
-		Polygon(Pt{0, 0}, true, layer9SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer9SpiralL, 0.0),
-		padLine(startLayer9R, innerViaPts[innerHole["9R"]]),
-		padLine(startLayer9L, innerViaPts[innerHole["9L"]]),
-		padLine(endLayer9R, outerViaPts[outerHole["9R"]]),
-		padLine(endLayer9L, outerViaPts[outerHole["9L"]]),
-	)
-	addVias(layer9)
-
-	layer11 := g.LayerN(11)
-	layer11.Add(
-		Polygon(Pt{0, 0}, true, layer11SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer11SpiralL, 0.0),
-		padLine(startLayer11R, innerViaPts[innerHole["11R"]]),
-		padLine(startLayer11L, innerViaPts[innerHole["11L"]]),
-		padLine(endLayer11R, outerViaPts[outerHole["11R"]]),
-		padLine(endLayer11L, outerViaPts[outerHole["11L"]]),
-	)
-	addVias(layer11)
-
-	layer13 := g.LayerN(13)
-	layer13.Add(
-		Polygon(Pt{0, 0}, true, layer13SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer13SpiralL, 0.0),
-		padLine(startLayer13R, innerViaPts[innerHole["13R"]]),
-		padLine(startLayer13L, innerViaPts[innerHole["13L"]]),
-		padLine(endLayer13R, outerViaPts[outerHole["13R"]]),
-		padLine(endLayer13L, outerViaPts[outerHole["13L"]]),
-	)
-	addVias(layer13)
-
-	layer15 := g.LayerN(15)
-	layer15.Add(
-		Polygon(Pt{0, 0}, true, layer15SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer15SpiralL, 0.0),
-		padLine(startLayer15R, innerViaPts[innerHole["15R"]]),
-		padLine(startLayer15L, innerViaPts[innerHole["15L"]]),
-		padLine(endLayer15R, outerViaPts[outerHole["15R"]]),
-		padLine(endLayer15L, outerViaPts[outerHole["15L"]]),
-	)
-	addVias(layer15)
-
-	layer17 := g.LayerN(17)
-	layer17.Add(
-		Polygon(Pt{0, 0}, true, layer17SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer17SpiralL, 0.0),
-		padLine(startLayer17R, innerViaPts[innerHole["17R"]]),
-		padLine(startLayer17L, innerViaPts[innerHole["17L"]]),
-		padLine(endLayer17R, outerViaPts[outerHole["17R"]]),
-		padLine(endLayer17L, outerViaPts[outerHole["17L"]]),
-	)
-	addVias(layer17)
-
-	layer19 := g.LayerN(19)
-	layer19.Add(
-		Polygon(Pt{0, 0}, true, layer19SpiralR, 0.0),
-		Polygon(Pt{0, 0}, true, layer19SpiralL, 0.0),
-		padLine(startLayer19R, innerViaPts[innerHole["19R"]]),
-		padLine(startLayer19L, innerViaPts[innerHole["19L"]]),
-		padLine(endLayer19R, outerViaPts[outerHole["19R"]]),
-		padLine(endLayer19L, outerViaPts[outerHole["19L"]]),
-	)
-	addVias(layer19)
 
 	outline := g.Outline()
 	r := 0.5*s.size + padD + *trace
