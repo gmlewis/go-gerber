db0267939c21fe61b273a77ee5adac4c0de6786b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-02-18 17:17:40 -0500

    Make layers more parametric

--- examples/icosi-bifilar-coil/main.go
+++ examples/icosi-bifilar-coil/main.go
@@ -130,46 +130,48 @@ func main() {
 		y := innerR * math.Sin(float64(i)*angleDelta)
 		innerViaPts = append(innerViaPts, Pt{x, y})
 	}
-	innerHoleTR := innerViaPts[17]
-	innerHoleTL := innerViaPts[7]
-	innerHoleBR := innerViaPts[13]
-	innerHoleBL := innerViaPts[3]
-	innerHole2R := innerViaPts[18]
-	innerHole2L := innerViaPts[8]
-	innerHole3R := innerViaPts[12]
-	innerHole3L := innerViaPts[2]
-	innerHole4R := innerViaPts[16]
-	innerHole4L := innerViaPts[6]
-	innerHole5R := innerViaPts[14]
-	innerHole5L := innerViaPts[4]
-	innerHole6R := innerViaPts[19]
-	innerHole6L := innerViaPts[9]
-	innerHole7R := innerViaPts[11]
-	innerHole7L := innerViaPts[1]
-	innerHole8R := innerViaPts[15]
-	innerHole8L := innerViaPts[5]
-	innerHole9R := innerViaPts[15]
-	innerHole9L := innerViaPts[5]
-	innerHole10R := innerViaPts[0]
-	innerHole10L := innerViaPts[10]
-	innerHole11R := innerViaPts[10]
-	innerHole11L := innerViaPts[0]
-	innerHole12R := innerViaPts[14]
-	innerHole12L := innerViaPts[4]
-	innerHole13R := innerViaPts[16]
-	innerHole13L := innerViaPts[6]
-	innerHole14R := innerViaPts[1]
-	innerHole14L := innerViaPts[11]
-	innerHole15R := innerViaPts[9]
-	innerHole15L := innerViaPts[19]
-	innerHole16R := innerViaPts[13]
-	innerHole16L := innerViaPts[3]
-	innerHole17R := innerViaPts[17]
-	innerHole17L := innerViaPts[7]
-	innerHole18R := innerViaPts[12]
-	innerHole18L := innerViaPts[2]
-	innerHole19R := innerViaPts[18]
-	innerHole19L := innerViaPts[8]
+	innerHole := map[string]int{
+		"TR":  17,
+		"TL":  7,
+		"BR":  13,
+		"BL":  3,
+		"2R":  18,
+		"2L":  8,
+		"3R":  12,
+		"3L":  2,
+		"4R":  16,
+		"4L":  6,
+		"5R":  14,
+		"5L":  4,
+		"6R":  19,
+		"6L":  9,
+		"7R":  11,
+		"7L":  1,
+		"8R":  15,
+		"8L":  5,
+		"9R":  15,
+		"9L":  5,
+		"10R": 0,
+		"10L": 10,
+		"11R": 10,
+		"11L": 0,
+		"12R": 14,
+		"12L": 4,
+		"13R": 16,
+		"13L": 6,
+		"14R": 1,
+		"14L": 11,
+		"15R": 9,
+		"15L": 19,
+		"16R": 13,
+		"16L": 3,
+		"17R": 17,
+		"17L": 7,
+		"18R": 12,
+		"18L": 2,
+		"19R": 18,
+		"19L": 8,
+	}
 
 	outerR := (2.0*math.Pi + float64(*n)*2.0*math.Pi + *trace + *gap) / (3.0 * math.Pi)
 	outerContactPt := func(n float64) Pt {
@@ -185,46 +187,48 @@ func main() {
 		outerViaPts = append(outerViaPts, pt)
 	}
 	outerViaPts = append(outerViaPts, outerContactPt(0.5))
-	outerHoleTR := outerViaPts[0]
-	outerHoleTL := outerViaPts[10]
-	outerHoleBR := outerViaPts[10]
-	outerHoleBL := outerViaPts[20]
-	outerHole2R := outerViaPts[1]
-	outerHole2L := outerViaPts[11]
-	outerHole3R := outerViaPts[9]
-	outerHole3L := outerViaPts[19]
-	outerHole4R := outerViaPts[19]
-	outerHole4L := outerViaPts[9]
-	outerHole5R := outerViaPts[11]
-	outerHole5L := outerViaPts[1]
-	outerHole6R := outerViaPts[2]
-	outerHole6L := outerViaPts[12]
-	outerHole7R := outerViaPts[8]
-	outerHole7L := outerViaPts[18]
-	outerHole8R := outerViaPts[18]
-	outerHole8L := outerViaPts[8]
-	outerHole9R := outerViaPts[12]
-	outerHole9L := outerViaPts[2]
-	outerHole10R := outerViaPts[3]
-	outerHole10L := outerViaPts[13]
-	outerHole11R := outerViaPts[7]
-	outerHole11L := outerViaPts[17]
-	outerHole12R := outerViaPts[17]
-	outerHole12L := outerViaPts[7]
-	outerHole13R := outerViaPts[13]
-	outerHole13L := outerViaPts[3]
-	outerHole14R := outerViaPts[4]
-	outerHole14L := outerViaPts[14]
-	outerHole15R := outerViaPts[6]
-	outerHole15L := outerViaPts[16]
-	outerHole16R := outerViaPts[16]
-	outerHole16L := outerViaPts[6]
-	outerHole17R := outerViaPts[14]
-	outerHole17L := outerViaPts[4]
-	outerHole18R := outerViaPts[15]
-	outerHole18L := outerViaPts[5]
-	outerHole19R := outerViaPts[15]
-	outerHole19L := outerViaPts[5]
+	outerHole := map[string]int{
+		"TR":  0,
+		"TL":  10,
+		"BR":  10,
+		"BL":  20,
+		"2R":  1,
+		"2L":  11,
+		"3R":  9,
+		"3L":  19,
+		"4R":  19,
+		"4L":  9,
+		"5R":  11,
+		"5L":  1,
+		"6R":  2,
+		"6L":  12,
+		"7R":  8,
+		"7L":  18,
+		"8R":  18,
+		"8L":  8,
+		"9R":  12,
+		"9L":  2,
+		"10R": 3,
+		"10L": 13,
+		"11R": 7,
+		"11L": 17,
+		"12R": 17,
+		"12L": 7,
+		"13R": 13,
+		"13L": 3,
+		"14R": 4,
+		"14L": 14,
+		"15R": 6,
+		"15L": 16,
+		"16R": 16,
+		"16L": 6,
+		"17R": 14,
+		"17L": 4,
+		"18R": 15,
+		"18L": 5,
+		"19R": 15,
+		"19L": 5,
+	}
 
 	drill := g.Drill()
 	for _, pt := range innerViaPts {
@@ -252,10 +256,10 @@ func main() {
 	top.Add(
 		Polygon(Pt{0, 0}, true, topSpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, topSpiralL, 0.0),
-		padLine(startTopR, innerHoleTR),
-		padLine(startTopL, innerHoleTL),
-		padLine(endTopR, outerHoleTR),
-		padLine(endTopL, outerHoleTL),
+		padLine(startTopR, innerViaPts[innerHole["TR"]]),
+		padLine(startTopL, innerViaPts[innerHole["TL"]]),
+		padLine(endTopR, outerViaPts[outerHole["TR"]]),
+		padLine(endTopL, outerViaPts[outerHole["TL"]]),
 	)
 	addVias(top)
 
@@ -266,10 +270,10 @@ func main() {
 	layer2.Add(
 		Polygon(Pt{0, 0}, true, layer2SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer2SpiralL, 0.0),
-		padLine(startLayer2R, innerHole2R),
-		padLine(startLayer2L, innerHole2L),
-		padLine(endLayer2R, outerHole2R),
-		padLine(endLayer2L, outerHole2L),
+		padLine(startLayer2R, innerViaPts[innerHole["2R"]]),
+		padLine(startLayer2L, innerViaPts[innerHole["2L"]]),
+		padLine(endLayer2R, outerViaPts[outerHole["2R"]]),
+		padLine(endLayer2L, outerViaPts[outerHole["2L"]]),
 	)
 	addVias(layer2)
 
@@ -277,10 +281,10 @@ func main() {
 	layer4.Add(
 		Polygon(Pt{0, 0}, true, layer4SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer4SpiralL, 0.0),
-		padLine(startLayer4R, innerHole4R),
-		padLine(startLayer4L, innerHole4L),
-		padLine(endLayer4R, outerHole4R),
-		padLine(endLayer4L, outerHole4L),
+		padLine(startLayer4R, innerViaPts[innerHole["4R"]]),
+		padLine(startLayer4L, innerViaPts[innerHole["4L"]]),
+		padLine(endLayer4R, outerViaPts[outerHole["4R"]]),
+		padLine(endLayer4L, outerViaPts[outerHole["4L"]]),
 	)
 	addVias(layer4)
 
@@ -288,10 +292,10 @@ func main() {
 	layer6.Add(
 		Polygon(Pt{0, 0}, true, layer6SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer6SpiralL, 0.0),
-		padLine(startLayer6R, innerHole6R),
-		padLine(startLayer6L, innerHole6L),
-		padLine(endLayer6R, outerHole6R),
-		padLine(endLayer6L, outerHole6L),
+		padLine(startLayer6R, innerViaPts[innerHole["6R"]]),
+		padLine(startLayer6L, innerViaPts[innerHole["6L"]]),
+		padLine(endLayer6R, outerViaPts[outerHole["6R"]]),
+		padLine(endLayer6L, outerViaPts[outerHole["6L"]]),
 	)
 	addVias(layer6)
 
@@ -299,10 +303,10 @@ func main() {
 	layer8.Add(
 		Polygon(Pt{0, 0}, true, layer8SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer8SpiralL, 0.0),
-		padLine(startLayer8R, innerHole8R),
-		padLine(startLayer8L, innerHole8L),
-		padLine(endLayer8R, outerHole8R),
-		padLine(endLayer8L, outerHole8L),
+		padLine(startLayer8R, innerViaPts[innerHole["8R"]]),
+		padLine(startLayer8L, innerViaPts[innerHole["8L"]]),
+		padLine(endLayer8R, outerViaPts[outerHole["8R"]]),
+		padLine(endLayer8L, outerViaPts[outerHole["8L"]]),
 	)
 	addVias(layer8)
 
@@ -310,10 +314,10 @@ func main() {
 	layer10.Add(
 		Polygon(Pt{0, 0}, true, layer10SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer10SpiralL, 0.0),
-		padLine(startLayer10R, innerHole10R),
-		padLine(startLayer10L, innerHole10L),
-		padLine(endLayer10R, outerHole10R),
-		padLine(endLayer10L, outerHole10L),
+		padLine(startLayer10R, innerViaPts[innerHole["10R"]]),
+		padLine(startLayer10L, innerViaPts[innerHole["10L"]]),
+		padLine(endLayer10R, outerViaPts[outerHole["10R"]]),
+		padLine(endLayer10L, outerViaPts[outerHole["10L"]]),
 	)
 	addVias(layer10)
 
@@ -321,10 +325,10 @@ func main() {
 	layer12.Add(
 		Polygon(Pt{0, 0}, true, layer12SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer12SpiralL, 0.0),
-		padLine(startLayer12R, innerHole12R),
-		padLine(startLayer12L, innerHole12L),
-		padLine(endLayer12R, outerHole12R),
-		padLine(endLayer12L, outerHole12L),
+		padLine(startLayer12R, innerViaPts[innerHole["12R"]]),
+		padLine(startLayer12L, innerViaPts[innerHole["12L"]]),
+		padLine(endLayer12R, outerViaPts[outerHole["12R"]]),
+		padLine(endLayer12L, outerViaPts[outerHole["12L"]]),
 	)
 	addVias(layer12)
 
@@ -332,10 +336,10 @@ func main() {
 	layer14.Add(
 		Polygon(Pt{0, 0}, true, layer14SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer14SpiralL, 0.0),
-		padLine(startLayer14R, innerHole14R),
-		padLine(startLayer14L, innerHole14L),
-		padLine(endLayer14R, outerHole14R),
-		padLine(endLayer14L, outerHole14L),
+		padLine(startLayer14R, innerViaPts[innerHole["14R"]]),
+		padLine(startLayer14L, innerViaPts[innerHole["14L"]]),
+		padLine(endLayer14R, outerViaPts[outerHole["14R"]]),
+		padLine(endLayer14L, outerViaPts[outerHole["14L"]]),
 	)
 	addVias(layer14)
 
@@ -343,10 +347,10 @@ func main() {
 	layer16.Add(
 		Polygon(Pt{0, 0}, true, layer16SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer16SpiralL, 0.0),
-		padLine(startLayer16R, innerHole16R),
-		padLine(startLayer16L, innerHole16L),
-		padLine(endLayer16R, outerHole16R),
-		padLine(endLayer16L, outerHole16L),
+		padLine(startLayer16R, innerViaPts[innerHole["16R"]]),
+		padLine(startLayer16L, innerViaPts[innerHole["16L"]]),
+		padLine(endLayer16R, outerViaPts[outerHole["16R"]]),
+		padLine(endLayer16L, outerViaPts[outerHole["16L"]]),
 	)
 	addVias(layer16)
 
@@ -354,10 +358,10 @@ func main() {
 	layer18.Add(
 		Polygon(Pt{0, 0}, true, layer18SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer18SpiralL, 0.0),
-		padLine(startLayer18R, innerHole18R),
-		padLine(startLayer18L, innerHole18L),
-		padLine(endLayer18R, outerHole18R),
-		padLine(endLayer18L, outerHole18L),
+		padLine(startLayer18R, innerViaPts[innerHole["18R"]]),
+		padLine(startLayer18L, innerViaPts[innerHole["18L"]]),
+		padLine(endLayer18R, outerViaPts[outerHole["18R"]]),
+		padLine(endLayer18L, outerViaPts[outerHole["18L"]]),
 	)
 	addVias(layer18)
 
@@ -365,10 +369,10 @@ func main() {
 	bottom.Add(
 		Polygon(Pt{0, 0}, true, botSpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, botSpiralL, 0.0),
-		padLine(startBotR, innerHoleBR),
-		padLine(startBotL, innerHoleBL),
-		padLine(endBotR, outerHoleBR),
-		padLine(endBotL, outerHoleBL),
+		padLine(startBotR, innerViaPts[innerHole["BR"]]),
+		padLine(startBotL, innerViaPts[innerHole["BL"]]),
+		padLine(endBotR, outerViaPts[outerHole["BR"]]),
+		padLine(endBotL, outerViaPts[outerHole["BL"]]),
 	)
 	addVias(bottom)
 
@@ -379,10 +383,10 @@ func main() {
 	layer3.Add(
 		Polygon(Pt{0, 0}, true, layer3SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer3SpiralL, 0.0),
-		padLine(startLayer3R, innerHole3R),
-		padLine(startLayer3L, innerHole3L),
-		padLine(endLayer3R, outerHole3R),
-		padLine(endLayer3L, outerHole3L),
+		padLine(startLayer3R, innerViaPts[innerHole["3R"]]),
+		padLine(startLayer3L, innerViaPts[innerHole["3L"]]),
+		padLine(endLayer3R, outerViaPts[outerHole["3R"]]),
+		padLine(endLayer3L, outerViaPts[outerHole["3L"]]),
 	)
 	addVias(layer3)
 
@@ -390,10 +394,10 @@ func main() {
 	layer5.Add(
 		Polygon(Pt{0, 0}, true, layer5SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer5SpiralL, 0.0),
-		padLine(startLayer5R, innerHole5R),
-		padLine(startLayer5L, innerHole5L),
-		padLine(endLayer5R, outerHole5R),
-		padLine(endLayer5L, outerHole5L),
+		padLine(startLayer5R, innerViaPts[innerHole["5R"]]),
+		padLine(startLayer5L, innerViaPts[innerHole["5L"]]),
+		padLine(endLayer5R, outerViaPts[outerHole["5R"]]),
+		padLine(endLayer5L, outerViaPts[outerHole["5L"]]),
 	)
 	addVias(layer5)
 
@@ -401,10 +405,10 @@ func main() {
 	layer7.Add(
 		Polygon(Pt{0, 0}, true, layer7SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer7SpiralL, 0.0),
-		padLine(startLayer7R, innerHole7R),
-		padLine(startLayer7L, innerHole7L),
-		padLine(endLayer7R, outerHole7R),
-		padLine(endLayer7L, outerHole7L),
+		padLine(startLayer7R, innerViaPts[innerHole["7R"]]),
+		padLine(startLayer7L, innerViaPts[innerHole["7L"]]),
+		padLine(endLayer7R, outerViaPts[outerHole["7R"]]),
+		padLine(endLayer7L, outerViaPts[outerHole["7L"]]),
 	)
 	addVias(layer7)
 
@@ -412,10 +416,10 @@ func main() {
 	layer9.Add(
 		Polygon(Pt{0, 0}, true, layer9SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer9SpiralL, 0.0),
-		padLine(startLayer9R, innerHole9R),
-		padLine(startLayer9L, innerHole9L),
-		padLine(endLayer9R, outerHole9R),
-		padLine(endLayer9L, outerHole9L),
+		padLine(startLayer9R, innerViaPts[innerHole["9R"]]),
+		padLine(startLayer9L, innerViaPts[innerHole["9L"]]),
+		padLine(endLayer9R, outerViaPts[outerHole["9R"]]),
+		padLine(endLayer9L, outerViaPts[outerHole["9L"]]),
 	)
 	addVias(layer9)
 
@@ -423,10 +427,10 @@ func main() {
 	layer11.Add(
 		Polygon(Pt{0, 0}, true, layer11SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer11SpiralL, 0.0),
-		padLine(startLayer11R, innerHole11R),
-		padLine(startLayer11L, innerHole11L),
-		padLine(endLayer11R, outerHole11R),
-		padLine(endLayer11L, outerHole11L),
+		padLine(startLayer11R, innerViaPts[innerHole["11R"]]),
+		padLine(startLayer11L, innerViaPts[innerHole["11L"]]),
+		padLine(endLayer11R, outerViaPts[outerHole["11R"]]),
+		padLine(endLayer11L, outerViaPts[outerHole["11L"]]),
 	)
 	addVias(layer11)
 
@@ -434,10 +438,10 @@ func main() {
 	layer13.Add(
 		Polygon(Pt{0, 0}, true, layer13SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer13SpiralL, 0.0),
-		padLine(startLayer13R, innerHole13R),
-		padLine(startLayer13L, innerHole13L),
-		padLine(endLayer13R, outerHole13R),
-		padLine(endLayer13L, outerHole13L),
+		padLine(startLayer13R, innerViaPts[innerHole["13R"]]),
+		padLine(startLayer13L, innerViaPts[innerHole["13L"]]),
+		padLine(endLayer13R, outerViaPts[outerHole["13R"]]),
+		padLine(endLayer13L, outerViaPts[outerHole["13L"]]),
 	)
 	addVias(layer13)
 
@@ -445,10 +449,10 @@ func main() {
 	layer15.Add(
 		Polygon(Pt{0, 0}, true, layer15SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer15SpiralL, 0.0),
-		padLine(startLayer15R, innerHole15R),
-		padLine(startLayer15L, innerHole15L),
-		padLine(endLayer15R, outerHole15R),
-		padLine(endLayer15L, outerHole15L),
+		padLine(startLayer15R, innerViaPts[innerHole["15R"]]),
+		padLine(startLayer15L, innerViaPts[innerHole["15L"]]),
+		padLine(endLayer15R, outerViaPts[outerHole["15R"]]),
+		padLine(endLayer15L, outerViaPts[outerHole["15L"]]),
 	)
 	addVias(layer15)
 
@@ -456,10 +460,10 @@ func main() {
 	layer17.Add(
 		Polygon(Pt{0, 0}, true, layer17SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer17SpiralL, 0.0),
-		padLine(startLayer17R, innerHole17R),
-		padLine(startLayer17L, innerHole17L),
-		padLine(endLayer17R, outerHole17R),
-		padLine(endLayer17L, outerHole17L),
+		padLine(startLayer17R, innerViaPts[innerHole["17R"]]),
+		padLine(startLayer17L, innerViaPts[innerHole["17L"]]),
+		padLine(endLayer17R, outerViaPts[outerHole["17R"]]),
+		padLine(endLayer17L, outerViaPts[outerHole["17L"]]),
 	)
 	addVias(layer17)
 
@@ -467,10 +471,10 @@ func main() {
 	layer19.Add(
 		Polygon(Pt{0, 0}, true, layer19SpiralR, 0.0),
 		Polygon(Pt{0, 0}, true, layer19SpiralL, 0.0),
-		padLine(startLayer19R, innerHole19R),
-		padLine(startLayer19L, innerHole19L),
-		padLine(endLayer19R, outerHole19R),
-		padLine(endLayer19L, outerHole19L),
+		padLine(startLayer19R, innerViaPts[innerHole["19R"]]),
+		padLine(startLayer19L, innerViaPts[innerHole["19L"]]),
+		padLine(endLayer19R, outerViaPts[outerHole["19R"]]),
+		padLine(endLayer19L, outerViaPts[outerHole["19L"]]),
 	)
 	addVias(layer19)
 
@@ -487,13 +491,15 @@ func main() {
 		outerLabelSize := 32.0 * r / 139.18 // determined emperically
 		message := fmt.Sprintf(messageFmt, *trace, *gap, *n)
 
-		innerLabel := func(label string, num float64) *TextT {
+		innerLabel := func(label string) *TextT {
+			num := float64(innerHole[label])
 			r := innerR - viaPadD
 			x := r * math.Cos(num*angleDelta)
 			y := r * math.Sin(num*angleDelta)
 			return Text(x, y, 1.0, label, *fontName, labelSize, &Center)
 		}
-		innerLabel2 := func(label string, num float64) *TextT {
+		innerLabel2 := func(label string) *TextT {
+			num := float64(innerHole[label])
 			r := innerR + viaPadD
 			x := r * math.Cos(num*angleDelta)
 			y := r * math.Sin(num*angleDelta)
@@ -515,46 +521,46 @@ func main() {
 		tss := g.TopSilkscreen()
 		tss.Add(
 			Text(0, 0.3*r, 1.0, message, *fontName, pts, &Center),
-			innerLabel("TR", 17),
-			innerLabel("TL", 7),
-			innerLabel("BR", 13),
-			innerLabel("BL", 3),
-			innerLabel("2R", 18),
-			innerLabel("2L", 8),
-			innerLabel("3R", 12),
-			innerLabel("3L", 2),
-			innerLabel("4R", 16),
-			innerLabel("4L", 6),
-			innerLabel("5R", 14),
-			innerLabel("5L", 4),
-			innerLabel("6R", 19),
-			innerLabel("6L", 9),
-			innerLabel("7R", 11),
-			innerLabel("7L", 1),
-			innerLabel("8R", 15),
-			innerLabel("8L", 5),
-			innerLabel2("9R", 15),
-			innerLabel2("9L", 5),
-			innerLabel("10R", 0),
-			innerLabel("10L", 10),
-			innerLabel2("11R", 10),
-			innerLabel2("11L", 0),
-			innerLabel2("12R", 14),
-			innerLabel2("12L", 4),
-			innerLabel2("13R", 16),
-			innerLabel2("13L", 6),
-			innerLabel2("14R", 1),
-			innerLabel2("14L", 11),
-			innerLabel2("15R", 9),
-			innerLabel2("15L", 19),
-			innerLabel2("16R", 13),
-			innerLabel2("16L", 3),
-			innerLabel2("17R", 17),
-			innerLabel2("17L", 7),
-			innerLabel2("18R", 12),
-			innerLabel2("18L", 2),
-			innerLabel2("19R", 18),
-			innerLabel2("19L", 8),
+			innerLabel("TR"),
+			innerLabel("TL"),
+			innerLabel("BR"),
+			innerLabel("BL"),
+			innerLabel("2R"),
+			innerLabel("2L"),
+			innerLabel("3R"),
+			innerLabel("3L"),
+			innerLabel("4R"),
+			innerLabel("4L"),
+			innerLabel("5R"),
+			innerLabel("5L"),
+			innerLabel("6R"),
+			innerLabel("6L"),
+			innerLabel("7R"),
+			innerLabel("7L"),
+			innerLabel("8R"),
+			innerLabel("8L"),
+			innerLabel2("9R"),
+			innerLabel2("9L"),
+			innerLabel("10R"),
+			innerLabel("10L"),
+			innerLabel2("11R"),
+			innerLabel2("11L"),
+			innerLabel2("12R"),
+			innerLabel2("12L"),
+			innerLabel2("13R"),
+			innerLabel2("13L"),
+			innerLabel2("14R"),
+			innerLabel2("14L"),
+			innerLabel2("15R"),
+			innerLabel2("15L"),
+			innerLabel2("16R"),
+			innerLabel2("16L"),
+			innerLabel2("17R"),
+			innerLabel2("17L"),
+			innerLabel2("18R"),
+			innerLabel2("18L"),
+			innerLabel2("19R"),
+			innerLabel2("19L"),
 
 			outerLabel2("TR", 0),
 			outerLabel("TL", 10),
